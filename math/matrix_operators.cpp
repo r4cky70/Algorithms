c3b3f01e8372b9e243fe0d34b2e0d35cc272f592
r4cky70 <rodrigo.estrela.sousa@ccc.ufcg.edu.br>
2019-09-17 23:49:49 -0300

    matrix_operations_finished

--- math/matrix_operators.cpp
+++ math/matrix_operators.cpp
@@ -32,26 +32,30 @@ matrix operator -(matrix a, matrix b){
   return a + (-1 * b);
 }
 
-matrix operator *(matrix a, matrix b){
-  int n = (int)a.size();
-  int m = (int)b.size();
-  int p = (int)b[0].size();
-  matrix c(n, vector<int>(p));
-  int aux[p];
-  for(int i = 0; i < p; ++i){
-    for(int j = 0; j < m; ++j){
-      int temp = 0;
-      for(int k = 0; k < a; ++k)
-      
-    }
-  }
+matrix operator *(matrix a, matrix b) {
+	int n = (int)a.size();
+	int m = (int)b.size();
+	int p = (int)b[0].size();
+	matrix c(n, vector<int>(p));
+	double col[m];
+	for (int j = 0; j < p; j++) {
+		for (int k = 0; k < m; k++)
+			col[k] = b[k][j];
+		for (int i = 0; i < n; i++) {
+			double s = 0;
+			for (int k = 0; k < m; k++)
+				s += a[i][k] * col[k];
+			c[i][j] = s;
+		}
+	}
+	return c;
 }
 
 
 int main(){
   matrix a = {{1, 2, 1},{2, 0, 0}};
-  matrix b = {{2, 0, 0}, {1, 1, 2}};
-  matrix c = a-b;
+  matrix b = {{2, 0,}, {1, 1,}, {0, 0}};
+  matrix c = a*b;
   for(int i = 0; i < c.size(); ++i){
     for(int j = 0; j < c[0].size(); ++j){
       cout << c[i][j] << " ";
